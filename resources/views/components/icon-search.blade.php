1ae5e3058782f289ef72a57ff1f2acd1c1d2525f
Gustavo Ayala <agustavo87@gmail.com>
2021-03-16 12:45:29 -0300

    Added view files

---
+++ resources/views/components/icon-search.blade.php
@@ -0,0 +1,6 @@
+<svg {{ $attributes }}  fill="currentColor" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g id="icon-search" stroke-width="1" fill-rule="evenodd">
+        <circle id="Oval-2" stroke-width="3" cx="10.5" cy="10.5" r="7"></circle>
+        <path d="M15.5,15.5 L21.5,21.5" id="Line" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+    </g>
+</svg>
