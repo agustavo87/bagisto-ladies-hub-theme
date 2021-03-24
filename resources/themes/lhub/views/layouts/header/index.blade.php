67710723ba3c6f8d2bc1d2d37fd7f4ad80ef8952
Gustavo Ayala <agustavo87@gmail.com>
2021-03-24 17:24:18 -0300

    FEAT: Dinamic menu hides/show by scroll

--- resources/themes/lhub/views/layouts/header/index.blade.php
+++ resources/themes/lhub/views/layouts/header/index.blade.php
@@ -412,6 +412,9 @@ if(auth()->guard('customer')->check()) {
     </script> 
 
     <script>
+            /************************************************** 
+             * Dinamic Navbar
+             **************************************************/
 
         class calculatePadding {
             constructor(headSelector) {
@@ -419,27 +422,156 @@ if(auth()->guard('customer')->check()) {
             }
             setPadding() {
                 let size = document.querySelector(this.headSelector).offsetHeight;
-                console.log(this.head);
-                console.log('size: ', size);
-
                 document.body.style.paddingTop=size + 'px';
-            }
-
-            doit() {
-                document.body.style.paddingTop='104px';
             }
         }
 
         const padCalc = new calculatePadding('#header');
         window.addEventListener('DOMContentLoaded', (event) => {
-            console.log('DOM fully loaded and parsed');
             padCalc.setPadding();
         });
 
         window.addEventListener('resize', () => {
-            console.log('resizing');
             padCalc.setPadding();
         })
+
+
+        class dinamicNavbar {
+            /**
+             * @param {string} navbar - Selector 
+             * @param {Object} options
+             * @param {number} options.disableWidth - Screen width in px <= at wich disable the styling of 
+             *                 the dinamic navbar.
+             */
+            constructor(navbar, options) {
+                this.elSelector = navbar;
+                this.options = options;
+                this.styleCache = new Map();
+                this.initialized = false;
+            }
+
+            /** @returns HTMLELement */
+            navEl() {
+                return document.querySelector(this.elSelector);
+            }
+
+            /**
+             *  Tries to init and return the initialization status
+             * 
+             * @returns {boolean}  false if not initialized */
+            init() {
+                if (this.initialized) {
+                    return true;
+                }
+
+                let el = this.navEl()
+                if(el.style.display === 'none' || this.outOfWidth()) {
+                    return false; // init height can't be calc if not dispalyed normally.
+                }
+
+                this.initHeight = el.offsetHeight;
+                this.height(this.initHeight);
+                this.recordHistory;
+                this.initialized = true;
+                return true;
+            }
+            
+            update() {
+                if (this.disabled()) return;
+                let calcHeight = this.limit(this.navEl().offsetHeight + this.getChange(1/2), this.initHeight, 0);
+                this.height(calcHeight);
+                this.recordHistory();
+            }
+
+            limit(n, max, min) {
+                return  (n < min) ? min :
+                        (n > max) ? max : 
+                        n;
+            }
+
+            recordHistory() {
+                this.lastPageYOffset = window.pageYOffset;
+            }
+
+            getChange(speed) {
+                return (this.lastPageYOffset - window.pageYOffset) * speed;
+            }
+
+            /** @param {number} h height in px */
+            height(h) {
+                this.setStyle('height', h + 'px');
+            }
+            
+            /**
+             * Sets style of the element and caches it,
+             * if it's not disabled.
+             * 
+             * @param {string} style
+             * @param {string} value
+             * @return {boolean} - false if is disabled, true otherwise.
+             */
+            setStyle(style, value) {
+                this.styleCache.set(style, value)
+                this.navEl().style[style] = value
+                return true;
+            }
+
+            responsiveController(event) {
+                if (this.disabled(event.target)) {
+                    // console.log('no permitido')
+                    this.clearStyles();
+                }
+                // } else {
+                //     console.log('permitido')
+                // }
+            }
+
+            clearStyles() {
+                let el = this.navEl();
+                this.styleCache.forEach((v, k) => el.style[k] = '');
+            }
+
+            /** 
+             * @param {Window} w -the window object 
+             * @returns {boolean}
+             * */
+            disabled(w = null) {
+                if (!this.init()) return true;
+                w = w ? w : window;
+                return this.outOfWidth(w);
+            }
+
+            /**
+             * Reuturns if the el. is outh of enable width
+             * 
+             * @returns {boolean}
+             */
+            outOfWidth(w = null) {
+                w = w ? w : window;
+                return w.document.body.clientWidth <= this.options.disableWidth;
+            }
+            
+            listen() {
+                window.addEventListener('scroll', () => {
+                    this.update();
+                })
+
+                window.addEventListener('DOMContentLoaded', (event) => {
+                    this.init(); // tries to init
+                })
+
+                window.addEventListener('resize', (e) => {
+                    this.responsiveController(e);
+                })
+            }
+
+        }
+
+        const navController = new dinamicNavbar('#header-bottom', {
+            disableWidth: 900 // width at witch swaps to mobile menu.
+        });
+        navController.listen();
+        
 
         $(document).ready(function() {
 
@@ -519,51 +651,7 @@ if(auth()->guard('customer')->check()) {
                 $('#compare-items-count').html(compareCount);
             @endguest
 
-            /************************************************** 
-             * Dinamic Navbar
-             **************************************************/
 
-            //  let initialHeaderBottomHeight = $('#header-bottom').height();
-            //  console.log('initialHeaderBottomHeight', initialHeaderBottomHeight);
-            //  $(window).scroll(function () {
-            //      let headerBottomHeight = initialHeaderBottomHeight - $(window).scrollTop();
-            //      console.log('headerBottomHeight A', headerBottomHeight);
-            //      headerBottomHeight = headerBottomHeight <= 0 ? 0 : headerBottomHeight;
-            //      console.log('headerBottomHeight B', headerBottomHeight);
-
-            //      $('#header-bottom').height(headerBottomHeight + 'px');
-            //  })
-
-            // window.addEventListener('scroll', function (event) {
-            //     console.log('window scrolled:', event)
-            // })
-
-            const headerBottom = document.querySelector('#header-bottom');
-            const header = document.querySelector('#header');
-            const headerInitialOffset = header.offsetTop;
-            const headerInitialWidth = header.clientWidth;
-            
-            // header.style.position = 'fixed';
-            // header.style.top = headerInitialOffset + 'px';
-            // header.style.width = headerInitialWidth + 'px';
-            // document.body.style.paddingTop = header.clientHeight + 'px';
-
-            
-            
-            // const headerBottomInitialHeight = headerBottom.clientHeight;
-            // headerBottom.style.height = headerBottomInitialHeight + 'px';
-            // window.addEventListener('scroll', () => {
-            //     let pageY = window.pageYOffset; 
-            //     let calcHeight = headerBottomInitialHeight - pageY;
-            //     calcHeight = calcHeight <= 0 ? 0 : calcHeight;
-
-            //     console.log('header-bottom A:', headerBottom.style.height);
-            //     console.log('top: ', pageY);
-            //     console.log('calcHeight: ', calcHeight);
-            //     headerBottom.style.height = calcHeight + 'px';
-            // });
-
-            
         });
     </script>
 @endpush
